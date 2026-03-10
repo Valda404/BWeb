a4701ef5672710d1bae83e4361720119a02e5556
Valda404 <pan.vlajka@gmail.com>
2026-03-10 23:23:20 +0100

    oprava kategorizace úkolů a přidání tlačítka na přesun  úkolu do nové kategorie plus koš

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -93,7 +93,7 @@ export function TaskList({ tasks, onToggleComplete, onDelete, onMoveToToday }) {
                   title="Smazat úkol"
                   className="p-1.5 rounded-lg text-gray-400 hover:text-red-500 hover:bg-red-50 transition-colors"
                 >
-                  <Trash className="w-4 h-4" />
+                  <Trash2 className="w-4 h-4" />
                 </button>
               </div>
             </div>
