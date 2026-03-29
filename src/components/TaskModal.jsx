8f8df890a7a4ab0dceaff2321920a8a2dbc9c071
Valda404 <pan.vlajka@gmail.com>
2026-03-29 20:14:58 +0200

    zalomení tlačítka pro export do kalendáře, protože funkce zatím nepracuje správně, řádek 162 - důležité chybu napravit jakmile dokončím bakalářkou práci a ukončím Feature Freeze

--- src/components/TaskModal.jsx
+++ src/components/TaskModal.jsx
@@ -159,13 +159,13 @@ export function TaskModal({ task, onClose, onSave, goals=[] }) {
 
             {/* Patička */}
             <div className="flex items-center justify-end gap-3 px-6 py-4 border-t border-gray-100 bg-gray-50">
-              <button
+              {/*<button
                 onClick={handleExportToCalendar}
                 className="flex items-center gap-1 px-4 py-2 text-sm text-gray-500 hover:text-gray-700 transition-colors"
               >
                 <CalendarPlus className="w-4 h-4" />
                 Přidat do Google kalendáře
-              </button>
+              </button>*/}
 
               <button
                 onClick={onClose}
